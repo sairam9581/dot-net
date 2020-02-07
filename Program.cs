6ec0a5270d8701f8c7e5234bb97c667cb2443827
Sairam <40314061+sairam9581@users.noreply.github.com>
2020-02-07 12:26:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+            string s = null ;
+            int l = str.Length;
+            char[] c = str.ToCharArray();
+            for(int i=l-1;i>=0;i--)
+            {
+                Console.Write("{0}", str[i]);
+                s = s + str[i];
+            }
+            Console.WriteLine("\n {0}",s);
+        }
+    }
+}
